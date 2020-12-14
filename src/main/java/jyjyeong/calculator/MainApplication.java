f07070e5b243ca3eb855cfa39ec5ff412e5244a8
jyjeong620 <jyjeong620@naver.com>
2020-12-14 11:25:05 +0900

    feat : main 실행파일 추가

---
+++ src/main/java/jyjyeong/calculator/MainApplication.java
@@ -0,0 +1,8 @@
+package jyjyeong.calculator;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.run();
+    }
+}
