c149e0358e26592f37f1aed2caffb4f9780a4495
jyjeong620 <jyjeong620@naver.com>
2020-12-16 21:12:18 +0900

    feat : 숫자 연산자 저장 추가

--- src/main/java/jyjyeong/calculator/Calculation.java
+++ src/main/java/jyjyeong/calculator/Calculation.java
@@ -13,7 +13,10 @@ public class Calculation {
     private static final String SUB = "-";
     private static final String MUL = "*";
     private static final String DIV = "/";
-    
+
+    private static int total = 0;
+    private static String nowOperator = "";
+
     public Calculation(String input) {
         this.input = splitInput(input);
     }
@@ -29,9 +32,39 @@ public class Calculation {
 
     //TODO 입력된 문자가 숫자인지 연산자인지 확인
 
-    //TODO 입력된 연산자가 유효한지 확인
+    /**
+     * 입력된 연산자가 유효한지 확인
+     */
+    private boolean isOperator(String operator){
+        boolean isOperator = false;
+        if(ADD.equals(operator)){
+            isOperator = true;
+        }
+        if(SUB.equals(operator)){
+            isOperator = true;
+        }
+        if(MUL.equals(operator)){
+            isOperator = true;
+        }
+        if(DIV.equals(operator)){
+            isOperator = true;
+        }
+        return isOperator;
+    }
 
+    /**
+     * 입력된 값을 현재 연산지로 지정한다
+     */
+    private void saveOperator(String operator){
+        nowOperator = operator;
+    }
 
+    /**
+     * 값을 받아와 최종 값에 추가해준다
+     */
+    private void plusTotal(int num){
+        total += num;
+    }
 
     /**
      * input 데이터를 공백으로 나눠 List 에 담는다
